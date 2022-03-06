134de6725c9b0fdf1fa233745c3bcf8a481b5fc6
Doktogreen <ajiboduraphael@gmail.com>
2022-03-06 01:21:16 -0800

    Progress..

--- src/views/components/home/carousel-section/index.js
+++ src/views/components/home/carousel-section/index.js
@@ -8,13 +8,20 @@ export default function CarouselSection(){
             <div className="carousel-inner">
                 <div className="carousel-item active">
                     <img className="w-100" src={carouselImgOne} alt="carousel-one" />
-                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                        <div className="p-3" style={{maxWidth: "900px"}}>
+                    <div className="carousel-caption d-flex flex-row align-items-center justify-content-between">
+                        <div className="p-0" style={{maxWidth: "650px", marginLeft: "9%", textAlign: "left"}}>
                             <h5 className="text-white text-uppercase mb-3 animated slideInDown">Creative & Innovative</h5>
+                            <h1 className="display-1 text-white mb-md-4 animated zoomIn">Creative & Innovative Digital Solution</h1>
+                            <a href="quote.html" className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
+                            <a href="..." className="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
+                        </div>
+                        <div className="p-3" style={{maxWidth: "700px"}}>
+                            <h5 className="text-white text-right text-uppercase mb-3 animated slideInDown">Creative & Innovative</h5>
                             <h1 className="display-1 text-white mb-md-4 animated zoomIn">Creative & Innovative Digital Solution</h1>
                             <a href="quote.html" className="btn  py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
                             <a href="..." className="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
                         </div>
+                        <div class="MuiBox-root css-137oubi peekaboo"><canvas width="700" height="657" style={{outline: "none", marginTop: "3rem", width: "700px", height: "657.5px"}}></canvas></div>
                     </div>
                 </div>
                 <div className="carousel-item">
