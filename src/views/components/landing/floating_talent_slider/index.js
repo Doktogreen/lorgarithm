94f68cc8143b4cafe9f2626e7e0eb2a9728d647a
Doktogreen <ajiboduraphael@gmail.com>
2022-03-21 02:34:04 -0700

    progress..

--- src/views/components/landing/floating_talent_slider/index.js
+++ src/views/components/landing/floating_talent_slider/index.js
@@ -3,30 +3,23 @@ import './index.css';
 
 export default function FloatingTalent(){
     return(
-        <div class="backg">
-        <div class="planet">
-            <div class="r1"></div>
-            <div class="r2"></div>
-            <div class="r3"></div>
-            <div class="r4"></div>
-            <div class="r5"></div>
-            <div class="r6"></div>
-            <div class="r7"></div>
-            <div class="r8"></div>
-            <div class="shad"></div>
+        <div class="container">
+            <div class="avatar">
+                <a href="https://codepen.io/MarioDesigns/">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/751678/skytsunami.png" alt="Skytsunami" />
+                </a>
+            </div>
+            <div class="content">
+                <h2>Floating CSS animation</h2>
+                <p>Follow me on:</p>
+                <p>
+                <span><a href="https://twitter.com/MDesignsuk" target="_blank"><i class="fa fa-twitter"></i></a></span>
+                <span><a href="https://github.com/Mario-Duarte/" target="_blank"><i class="fa fa-github"></i></a></span>
+                <span><a href="https://bitbucket.org/Mario_Duarte/" target="_blank"><i class="fa fa-bitbucket"></i></a></span>
+                <span><a href="https://codepen.io/MarioDesigns/" target="_blank"><i class="fa fa-codepen"></i></a></span>
+                </p>
+                <p>BY: Mario Duarte</p>
+            </div>
         </div>
-        <div class="stars">
-            <div class="s1"></div>
-            <div class="s2"></div>
-            <div class="s3"></div>
-            <div class="s4"></div>
-            <div class="s5"></div>
-            <div class="s6"></div>
-        </div>
-        <div class="an">
-        <div class="tank"></div>
-        <img className='astro' src="https://res.cloudinary.com/skiltime/image/upload/v1646493069/LorgarithmBank4_p03yuu.png" alt='talents' />
-      </div>
-    </div>
     )
-}
+};
